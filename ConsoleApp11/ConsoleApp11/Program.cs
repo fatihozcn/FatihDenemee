3824db5b49c47f4a048a13a2d096e1d4b9d5e226
Fatih Özcan <fatih_ozcan0505@hotmail.com>
2020-11-07 01:50:28 +0300

    Gaziyi ekledim

--- ConsoleApp11/ConsoleApp11/Program.cs
+++ ConsoleApp11/ConsoleApp11/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp11
         static void Main(string[] args)
         {
             Console.WriteLine("Hello 313123 Ahmet!");
+            Console.WriteLine("Hello Gazi!");
         }
     }
 }
